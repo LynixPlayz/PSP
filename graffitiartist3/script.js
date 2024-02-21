d0af6fe44f968576e7cf546e0037034fcc7e93e9
Alexander Wilson <82721356+LynixPlayz@users.noreply.github.com>
2024-02-21 09:51:06 -0600

    ⚙️ Misc Changes

--- graffitiartist3/script.js
+++ graffitiartist3/script.js
@@ -1,5 +1,5 @@
 $("#choice1").on("click", function(){
-    window.location = window.location.origin + window.location.pathname.replace("2", "3") + window.location.search + "a" 
+    window.location = window.location.origin + window.location.pathname.replace("graffitiartist3", "itsthefinalcountdown") + window.location.search + "a" 
 });
 
 $("#choice2").on("click", function(){
